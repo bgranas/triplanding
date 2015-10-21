56d7631803c8d5e5966d4c6d28ae3556dc8dadc4
Calvin Hawkes <calvinhawkes@gmail.com>
2015-10-21 23:35:40 +0800

    default CRUD implemented

---
+++ app/controllers/leads_controller.rb
@@ -0,0 +1,19 @@
+class LeadsController < ApplicationController
+
+  layout false
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
--- db/migrate/20151021143609_create_leads.rb
+++ db/migrate/20151021143609_create_leads.rb
@@ -1,7 +1,7 @@
 class CreateLeads < ActiveRecord::Migration
   def change
     create_table :leads do |t|
-
+      t.string "email", :null => false
       t.timestamps null: false
     end
   end
