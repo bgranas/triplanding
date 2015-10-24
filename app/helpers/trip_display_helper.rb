b9fc48eb9156464e4dcea72d33bd19066ca8529c
Ben Granas <benjamingranas@gmail.com>
2015-10-24 15:31:30 +0800

    Beta index page

---
+++ app/helpers/trip_display_helper.rb
@@ -0,0 +1,2 @@
+module TripDisplayHelper
+end
---
+++ test/controllers/trip_display_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TripDisplayControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
