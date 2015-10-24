260f89298d05471973559ac6725c544b94471598
Calvin Hawkes <calvinhawkes@gmail.com>
2015-10-24 15:29:31 +0800

    redirects to previous url on sign in, registration, or sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  after_filter :store_location
+
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+
+  #after login, redirect to previous page, or the root URL if session is not defined
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+
+  #keep user on same page after signout
+  def after_sign_out_path_for(user)
+    request.referrer
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,7 +11,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def after_sign_up_path_for(user)
-    '/beta'
+    session[:previous_url] || root_path
   end
 
 end
