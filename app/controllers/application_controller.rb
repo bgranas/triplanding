72e5695c7d78cc5bdff89da75fde65d429926ffc
Ben Granas <benjamingranas@gmail.com>
2015-10-25 10:19:02 +0800

    Remove helper all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
 
   after_filter :store_location
 
-  helper :all
-
   def store_location
     # store last url as long as it isn't a /users path
     session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
