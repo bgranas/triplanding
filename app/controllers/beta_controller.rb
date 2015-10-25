1d1682925ed5e1e0330a6848fe690606d56cd6aa
Ben Granas <benjamingranas@gmail.com>
2015-10-25 09:14:18 +0800

    Trip Display widget

--- app/controllers/beta_controller.rb
+++ app/controllers/beta_controller.rb
@@ -1,6 +1,14 @@
 class BetaController < ApplicationController
   def index
     @transparent_nav = true
+
+    #Trip Display variables
+    @tripTitle = "Vietnam North to South"
+		@tripMapStatic = "https://www.google.com/maps/api/staticmap?size=275x300&path=weight:5|Hanoi,Vietnam|Halong,Vietnam|Hue,Vietnam|DaLat,Vietnam|HoChiMinhCity,Vietnam"
+		@tripCountry = "Vietnam"
+		@upvoteScore = 10
+		@username = "User12345"
+
   end
 
   def about
