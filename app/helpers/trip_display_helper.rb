bbcbc273a519d4b91ce9a2e676b07dcc14f98819
Calvin Hawkes <calvinhawkes@gmail.com>
2015-10-25 10:09:30 +0800

    trip display helper working

--- app/helpers/trip_display_helper.rb
+++ app/helpers/trip_display_helper.rb
@@ -1,6 +1,6 @@
 module TripDisplayHelper
 
-	def tripdisplay
+	def trip_display
 		@tripTitle = "Vietnam North to South"
 		@tripMapStatic = "https://www.google.com/maps/api/staticmap?size=275x300&path=weight:5|Hanoi,Vietnam|Halong,Vietnam|Hue,Vietnam|DaLat,Vietnam|HoChiMinhCity,Vietnam"
 		@tripCountry = "Vietnam"
