18203b5476956923757c628a169a02d8967efc2b
Calvin Hawkes <calvinhawkes@gmail.com>
2015-10-26 12:30:22 +0800

    signup form works on lightbox

---
+++ app/assets/javascripts/application_wide.coffee
@@ -0,0 +1,5 @@
+#Created by Calvin - for application-wide coffeescript
+
+$ ->
+  $('.lightbox-close').click ->
+    $.colorbox.close()
---
+++ app/controllers/blank_controller.rb
@@ -0,0 +1,6 @@
+class BlankController < ApplicationController
+
+  #controller with the sole purpose of rendering partials in lightboxes
+  layout 'blank'
+
+end
