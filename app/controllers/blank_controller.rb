e5ac5898206e9a4936a57a8500899d6bdefd23ab
Calvin Hawkes <calvinhawkes@gmail.com>
2015-10-26 21:53:36 +0800

    ajax login doesn't return dummy html

--- app/controllers/blank_controller.rb
+++ app/controllers/blank_controller.rb
@@ -3,4 +3,7 @@ class BlankController < ApplicationController
   #controller with the sole purpose of rendering partials in lightboxes
   layout 'blank'
 
+  def index
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,17 @@
 class SessionsController < Devise::SessionsController
-    respond_to :json
+  respond_to :html, :json
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+
+
+    if request.format.html?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      return render :json => {:success => true, :redirect => after_sign_in_path_for(resource)}
+    end
+  end
 end
