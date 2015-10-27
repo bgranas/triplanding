cfdb13dd7d18d68480dab030d71487a0cc319c9b
Calvin Hawkes <calvinhawkes@gmail.com>
2015-10-27 12:21:03 +0800

    errors display correctly signup

--- app/assets/javascripts/application_wide.coffee
+++ app/assets/javascripts/application_wide.coffee
@@ -6,11 +6,39 @@ $ ->
   $(document).bind 'cbox_load', ->
     $('#colorbox').blur()
 
+
+  #after colorbox completes, bind to sign-up form
+  $(document).bind 'cbox_complete', ->
+    #sign-up success or error actions
+    $("#signup-form").on "ajax:success", (e, data, status, xhr) ->
+      alert 'ajax successful'
+      if data.success #signed-up
+        location.reload()
+      else #failed sign-up
+        $('#signup-container').find('.error-message').html('')
+        alert 'data.success = failed. errors: ' + data.errors
+        for error in data.errors
+          do ->
+            $('#signup-container').find('.error-message').append('<li>' + error + '</li>')
+
+
+      #location.reload()
+    $("#signup-form").on "ajax:error", (e, xhr, status, error) ->
+      $('#signup-container').find('.error-message').html('')
+      data = JSON.parse(xhr.responseText)
+      for e in data.errors
+          do ->
+            $('#signup-container').find('.error-message').append('<li>' + e + '</li>')
+
+
+
   #login success or error actions
   $("#login-form").on "ajax:success", (e, data, status, xhr) ->
     location.reload()
   $("#login-form").on "ajax:error", (e, xhr, status, error) ->
     $('#login-container').find('.error-message').text(xhr.responseText)
+
+
 
 
   $('.dropdown-toggle-hover').on('mouseover', ->
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,58 @@
 class RegistrationsController < Devise::RegistrationsController
+  respond_to :html, :json
+
+
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      puts '************************sign-up failed'
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      puts '************************sign-up failed. format: ' + request.format
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_to do |format|
+        format.html {respond_with resource}
+        format.js { return render :json => {:status => 400, :success => false, :errors => resource.errors.full_messages } }
+      end
+    end
+=begin
+
+    build_resource
+
+    if resource.save
+      puts '************************sign-up failed'
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        return render :json => {:success => true}
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        return render :json => {:success => true}
+      end
+    else
+      puts '************************sign-up failed'
+      clean_up_passwords resource
+      return render :json => {:status => 400, :success => false, :errors => resource.errors.full_messages }
+    end
+
+=end
+  end
+
+
 
   private
 
