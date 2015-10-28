7340bb191fc5a202b0d76b6c425a5fca134f8496
Calvin Hawkes <calvinhawkes@gmail.com>
2015-10-28 09:16:19 +0800

    static page tests written

--- test/controllers/beta_controller_test.rb
+++ test/controllers/beta_controller_test.rb
@@ -6,7 +6,6 @@ class BetaControllerTest < ActionController::TestCase
     assert true
   end
 
-
   test "should get index" do
     get :index
     assert_response :success
@@ -19,6 +18,36 @@ class BetaControllerTest < ActionController::TestCase
 
   test "should get contact" do
     get :contact
+    assert_response :success
+  end
+
+  test "should get how it works" do
+    get :how_it_works
+    assert_response :success
+  end
+
+  test "should get blog" do
+    get :blog
+    assert_response :success
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+  end
+
+  test "should get site policies" do
+    get :policies
+    assert_response :success
+  end
+
+  test "should get privacy policy" do
+    get :privacy
+    assert_response :success
+  end
+
+  test "should get terms of service" do
+    get :terms
     assert_response :success
   end
 
