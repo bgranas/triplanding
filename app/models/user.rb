1f7f79fe214dfc6516fc8bd2ee87b672de82059f
Calvin Hawkes <calvinhawkes@gmail.com>
2015-10-29 23:50:25 +0800

    generates profile URL from name, if name is taken, generates a number after

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   NULL_ATTRS = %w( hometown country_iso_3 country blog_url profile_url profile_picture_path )
 
   before_validation :nil_if_blank
+  before_validation :generate_profile_url
 
 
 
@@ -27,6 +28,29 @@ class User < ActiveRecord::Base
   def nil_if_blank
     #puts "******** nil_if_blank called"
     NULL_ATTRS.each { |attr| self[attr] = nil if self[attr].blank? }
+  end
+
+  #generating unique profile url if one was not provided
+  def generate_profile_url
+    debug = false
+    if self.profile_url.blank?
+      self.profile_url = self.name
+      self.valid? #testing to see if self is valid after profile url
+      puts '************** profile_url is taken' if debug
+      puts '************** self.errors[profile_url]: ' + self.errors[:profile_url].to_s if debug
+
+      #there can be other reasons that the user is invalid, so we just want to keep checking
+      #until there are no errors in the profile_url
+      i = 1
+      while not self.errors[:profile_url].blank? do
+        puts '************** self.errors[profile_url]: ' + self.errors[:profile_url].to_s if debug
+        self.profile_url = self.name + "_" + i.to_s
+        puts '************** new profile_url ' + self.profile_url if debug
+        self.valid? #checking if valid, will generate new self.errors
+        i = i + 1
+      end
+
+    end
   end
 
 
