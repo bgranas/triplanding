61296f7c660fdba7a75d59d8c0efe3cd14822e31
Ben Granas <benjamingranas@gmail.com>
2015-10-29 10:39:37 +0800

    User test cases

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -207,4 +207,21 @@ class UserTest < ActiveSupport::TestCase
 
   end
 
+  #test users/index only loads for admin_user
+
+  #test user id in correct format must be present for show
+
+  #test show loads with profile URL
+
+  #test show does not load with incorrect profile URL
+
+  #test show will still load with nil profile picture
+
+  #test show will still load with nil hometown
+
+  #test show will still load with nil profile picture
+
+  #test show will still load with nil blog_url
+
+
 end
