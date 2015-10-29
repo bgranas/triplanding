7dcb3250e3906270ab7cfa85b5e5af08f2802347
Calvin Hawkes <calvinhawkes@gmail.com>
2015-10-29 23:26:04 +0800

    wrote some more tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,7 +4,7 @@ class UsersControllerTest < ActionController::TestCase
 
 
   def setup
-    #@request.env["devise.mapping"] = Devise.mappings[:user]
+    @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
 
@@ -14,65 +14,53 @@ class UsersControllerTest < ActionController::TestCase
     end
   end
 
-
-  test "can't edit a user with a different id" do
+  test "autogenerate profile_url" do
     assert false, "need to write test"
+    #should test that it autogenerates, and comes up with smart default if the names are the same
   end
 
+   test "users/show with id should fail" do
+    assert_raise ActiveRecord::RecordNotFound, "successfully accessed users/show/1" do
+      get :show, {:id => users(:one).id}
+    end
 
-  test "user id in correct format must be present for show" do
-    get :show, {:profile_url => 'uncy_c'}
-    assert_response :success, "cannot get /uncy_c, should be valid"
-
-    assert_raise ActiveRecord::RecordNotFound, "successfully got /users/show/69, should be invalid" do
-      get :show, {:profile_url => 'not_a_url'}
+    assert_raise ActiveRecord::RecordNotFound, "successfully accessed users/show/1" do
+      get :show, {:id => 1}
     end
   end
 
-  test "show loads with profile URL" do
-    assert false, "need to write test"
+
+  test "can only edit user with the same id" do
+    sign_in users(:one) #id = 1
+    get :edit, {:id => users(:one).id}
+    assert_response :success
+
+    assert_raise ActiveRecord::RecordNotFound, "successfully got /users/edit/2, id=2 does not exist" do
+      get :edit, {:id => 69}
+    end
+
+    get :edit, {:id => users(:two).id}
+    assert_response :redirect, "able to edit user with different id"
   end
 
-  test "show does not load with incorrect profile URL" do
-    assert false, "need to write test"
-  end
 
-  test "show will still load with nil profile picture" do
-    assert false, "need to write test"
-  end
+  test "getting user profile via profile_url" do
+    get :show, {:profile_url => 'uncy_c'}
+    assert_response :success, "cannot get /uncy_c, should be valid"
 
-  test "show will still load with nil hometown" do
-    assert false, "need to write test"
-  end
+    assert_raise ActiveRecord::RecordNotFound, "successfully got profile url that doesn't exist" do
+      get :show, {:profile_url => 'not_a_url'}
+    end
 
-  test "show will still load with nil blog_url" do
-    assert false, "need to write test"
+    assert_raise ActiveRecord::RecordNotFound, "successfully got /users/show/69, should be invalid" do
+      get :show, {:profile_url => ''}
+    end
   end
 
   test "should not index if not logged in" do
     assert_raise AbstractController::ActionNotFound, "successfully got /users/index, should be invalid" do
       get :index
     end
-  end
-
-  test "should save edit with blog_url as empty string" do
-    assert false, "need to write test"
-  end
-
-  test "should save edit with profile_url as empty string" do
-    assert false, "need to write test"
-  end
-
-  test "should save edit with profile_picture_path as empty string" do
-    assert false, "need to write test"
-  end
-
-  test "should save edit with hometown as empty string" do
-    assert false, "need to write test"
-  end
-
-  test "should save edit with country as empty string" do
-    assert false, "need to write test"
   end
 
 end
---
+++ test/integration/user_profile_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+
+
+class UserProfileTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @test_user = User.new ({:name => 'test_user', :email => 'test@user.com', :password => '12345678', :password_confirmation => '12345678'})
+    @test_user.save!
+
+    @admin_user = User.new ({:name => 'admin_user', :email => 'admin@user.com', :password => '12345678', :password_confirmation => '12345678'})
+    @admin_user.save!
+    @admin_user.isAdmin = true
+    @admin_user.save!
+  end
+
+  test "profile picture corrently defaults if not defined" do
+    assert false, "need to write test"
+  end
+
+  test "blog url links to correct location" do
+    assert false, "need to write test"
+  end
+
+  test "edit user profile should preload values" do
+    assert false, "need to write test"
+  end
+
+  test "should save edit with blog_url as empty string" do
+    login_as(@test_user)
+    visit '/users/edit/' + @test_user.id.to_s
+
+    assert_current_path '/users/edit/' + @test_user.id.to_s
+    #within('form') do
+      #fill_in 'user_name'
+    #end
+  end
+
+  test "should save edit with profile_url as empty string" do
+    assert false, "need to write test"
+  end
+
+  test "should save edit with profile_picture_path as empty string" do
+    assert false, "need to write test"
+  end
+
+  test "should save edit with hometown as empty string" do
+    assert false, "need to write test"
+  end
+
+  test "should save edit with country as empty string" do
+    assert false, "need to write test"
+  end
+
+end
