d7fccc8de834dcc3fe4706b22ce79f08cb331003
Calvin Hawkes <calvinhawkes@gmail.com>
2015-10-30 10:34:41 +0800

    fixed stack overflow error for names with spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  NULL_ATTRS = %w( hometown country_iso_3 country blog_url profile_url profile_picture_path )
+  NULL_ATTRS = %w( hometown country_iso_3 country blog_url profile_url profile_picture_path name email )
 
   before_validation :nil_if_blank
+
+  #WARNING: This must be run after nil_if_blank is run
   before_validation :generate_profile_url
 
 
@@ -30,21 +32,25 @@ class User < ActiveRecord::Base
     NULL_ATTRS.each { |attr| self[attr] = nil if self[attr].blank? }
   end
 
+  #pre: nil_if_blank must be run otherwise a name with empty spaces will cause SystemStackError: stack level too deep if user
   #generating unique profile url if one was not provided
   def generate_profile_url
     #need to replace spaces with strings
-    debug = true
-    if self.profile_url.blank?
-      self.profile_url = self.name
+    debug = false
+
+    if !self.name.nil? and self.profile_url.blank?
+      self.profile_url = self.name #THIS LINE FAILS IF self.name is true
       self.valid? #testing to see if self is valid after profile url
 
       puts '************** user is not valid, but does the error have to do with profile_url?' if debug
       puts '************** self.errors[profile_url]: ' + self.errors[:profile_url].to_s if debug
+      puts '************** User.find_by_profile_url(self.profile_url).nil?: ' +
+                            (User.find_by_profile_url(self.profile_url).nil?).to_s if debug
 
       #there can be other reasons that the user is invalid, so we just want to keep checking
       #until there are no errors in the profile_url
-      i = 1
-      while self.errors[:profile_url] == "has already been taken" do
+      i = 2
+      while not User.find_by_profile_url(self.profile_url).nil? do
         puts '************** self.errors[profile_url]: ' + self.errors[:profile_url].to_s if debug
         self.profile_url = self.name + "_" + i.to_s
         puts '************** new profile_url ' + self.profile_url if debug
