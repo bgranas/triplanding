dab89abbed3fd226a1fed52b8f9724c1b3e27cdc
Calvin Hawkes <calvinhawkes@gmail.com>
2015-11-11 14:28:15 +0800

    fixed map null problem?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,35 +21,6 @@
 //= require_tree .
 
 
-var map;
-var bounds;
-var polyline;
-var infowindow;
-var myLatLng = {lat: 28, lng: 0};
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map-holder'), {
-    center: myLatLng,
-    zoom: 3,
-    scrollwheel: true,
-    streetViewControl: false,
-    panControl: false,
-    mapTypeControlOptions: {position: google.maps.ControlPosition.TOP_RIGHT},
-    zoomControlOptions: {position: google.maps.ControlPosition.RIGHT_TOP}
-  });
-
-  polyline = new google.maps.Polyline({
-    geodesic: true,
-    map: map
-  });
-
-  var contentString ='<h3>Sample Content Yo</h3>'
-
-  infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  bounds = new google.maps.LatLngBounds();
-}
 
 
 
