41cc5acaf62fd71f2dd679572dd768589bafe8bd
Ben Granas <benjamingranas@gmail.com>
2015-11-13 23:04:52 +0800

    Revert add_photo_url migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113125756) do
+ActiveRecord::Schema.define(version: 20151107034524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 20151113125756) do
     t.string   "ward"
     t.datetime "created_at",                                           null: false
     t.datetime "updated_at",                                           null: false
-    t.string   "photoURL"
   end
 
   add_index "destinations", ["google_place_id"], name: "index_destinations_on_google_place_id", using: :btree
