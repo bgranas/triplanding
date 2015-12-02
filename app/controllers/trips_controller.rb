4478b2aca79820f54ed2be92f1ac3d7daa4361b1
Calvin Hawkes <calvinhawkes@gmail.com>
2015-12-02 20:04:19 +0700

    clean up old code now that we have itinerary

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -53,46 +53,7 @@ class TripsController < ApplicationController
     # add initialization script for google maps
     @map_page = true
 
-
-
-
-
     @trip = Trip.create #creates/saves a trip object
-
-
-    @itineraryOriginDate = "1"
-    @itineraryOriginTransportation = "Flight from SFO to PVG"
-    @itineraryOriginSpecifics = "American Airlines flight 1 | 9:00 AM"
-    @itineraryOriginPrice = "$1000"
-
-
-    @itineraryStepOrder = "1"
-    @itineraryStepDate = "2"
-    @itineraryStepCity = "Shanghai"
-    @itineraryStepCountry = "CHINA"
-
-    @accommodationType = "Hotel"
-    @accommodationSpecifics = "2 nights in ABC Hotel | Check in: Jan 1 Check out: Jan 3"
-    @accommodationPrice = "$100"
-
-    #if true renders destination selection in itinerary, if false renders "add next destination" button
-    @destinationChosen = true
-    #if true renders transport selection in itinerary, if false renders transport search with r2r call
-    @transportChosen = true
-    #if true renders accomm selection in itinerary, if false renders accomm search
-    @accommChosen = true
-
-
-
-    @transportationType = "Bus"
-    @itineraryStepDestination2 = "Dafeng"
-    @transportationSpecifics = "DF Bus | Shanghai Bus Station | 12:00 PM"
-    @transportationPrice = "$15"
-
-
-    #calling TransportationHelper
-    #r2r_call
-
   end
 
   def create
