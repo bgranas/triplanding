aa2a3fa02b9f7955e6475bab4be9e065745069c9
Ben Granas <benjamingranas@gmail.com>
2015-12-05 10:54:52 -0500

    Script to import countries to country table

---
+++ lib/tasks/import_countries.rb
@@ -0,0 +1,8 @@
+  desc "Import countries." 
+  task :import_countries => :environment do
+    File.open("countries.txt", "r").each do |line|
+      name, region, sub_region = line.strip.split("\t")
+      #c = Country.new(:name => name, :region => region, :sub_region => sub_region)
+      # => c.save
+    end
+  end
